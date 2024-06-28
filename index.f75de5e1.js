a17bae2117921c5a7783d757cc758d31219b4768
Tania Kuzmenko <tanch.sergeevich@gmail.com>
2024-06-29 02:46:07 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,18 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+document.addEventListener("click", (e)=>{
+    const wallSpace = wall.getBoundingClientRect();
+    const spiderSpace = spider.getBoundingClientRect();
+    const wallStyle = getComputedStyle(wall);
+    const borderHorWidth = parseFloat(wallStyle.borderLeftWidth) + parseFloat(wallStyle.borderRightWidth);
+    const borderVertWidth = parseFloat(wallStyle.borderTopWidth) + parseFloat(wallStyle.borderBottomWidth);
+    const clickX = e.clientX - wallSpace.left;
+    const clickY = e.clientY - wallSpace.top;
+    const newLeft = Math.min(Math.max(clickX - spiderSpace.width / 2 - borderHorWidth / 2, 0), wallSpace.width - spiderSpace.width - borderHorWidth);
+    const newTop = Math.min(Math.max(clickY - spiderSpace.height / 2 - borderHorWidth / 2, 0), wallSpace.height - spiderSpace.height - borderVertWidth);
+    spider.style.left = `${newLeft}px`;
+    spider.style.top = `${newTop}px`;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
